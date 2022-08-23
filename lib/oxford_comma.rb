82a4e4d92bdd8ae15c4b5da95b47bb7642468121
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-08-23 22:03:45 +0300

    commit tree 80383c4e63f048fcbe3b07df31743e34845ec779

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+# takes an array of string elements as an argument and converts it into a string using the Oxford comma
+    if array.length == 2
+        return array.join(" and ")  
+    elsif array.length >= 3
+        array.insert(-2, "and")
+        return array.join(", ")  
+    else
+        return array[0]
+   
+    end
 end
